4c552990f24ba2872389eebde75c3bcb095c99df
ziababar <zbabar@gmail.com>
2018-04-12 22:38:17 -0400

    Add project files.

---
+++ IdentityManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdentityManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
