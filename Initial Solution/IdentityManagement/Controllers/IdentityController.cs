3b60dd41cf611be09394f4869aff3da26427680b
Zia Babar <zbabar@gmail.com>
2018-08-29 15:16:40 -0400

    reorg
    reorg

--- Initial Solution/IdentityManagement/Controllers/IdentityController.cs
+++ Initial Solution/IdentityManagement/Controllers/IdentityController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Mvc;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel;
 
 
 namespace IdentityManagement.Controllers
